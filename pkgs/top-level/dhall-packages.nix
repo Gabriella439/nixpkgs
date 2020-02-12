e9ff793eeb3b566943bfbddb4040018037cbfd90
Gabriel Gonzalez <Gabriel439@gmail.com>
2020-02-11 21:01:47 -0800

    Add Nixpkgs support for Dhall

---
+++ pkgs/top-level/dhall-packages.nix
@@ -0,0 +1,25 @@
+{ lib
+, newScope
+, overrides ? (self: super: {})
+}:
+
+let
+  packages = self:
+    let
+      callPackage = newScope self;
+
+      buildDhallPackage =
+        callPackage ../development/interpreters/dhall/build-dhall-package.nix { };
+
+    in
+      { inherit buildDhallPackage;
+
+        dhall-kubernetes =
+          callPackage ../development/dhall-modules/dhall-kubernetes.nix { };
+
+        Prelude =
+          callPackage ../development/dhall-modules/Prelude.nix { };
+      };
+
+in
+  lib.fix' (lib.extends overrides packages)
